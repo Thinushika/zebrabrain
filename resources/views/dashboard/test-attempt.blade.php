0c89f8889997e7c3c98f3105d4cce4caf0b7e5a4
tj <thinupavani@gmail.com>
2024-02-09 17:07:36 +0530

    test attempt page

--- resources/views/dashboard/test-attempt.blade.php
+++ resources/views/dashboard/test-attempt.blade.php
@@ -1,14 +1,131 @@
 @include('layouts.dashboard-header')
-<div class="dashboard-page-content pe-5">
+<div class="dashboard-page-content">
 
-    <h3 class="section-title text-purple mt-3">
-        My brain test attempts
-    </h3>
-    <p class="section-description-intentship mt-1 pe-5">
-        You can see latest 10 attempts only. Other older attempts will be deleted automatically.
-    </p>
-    <div class="d-flex flex-row justify-content-between ">
-       
+    <div class="d-flex flex-row">
+        <div class="col-7">
+            <h3 class="section-title text-purple mt-3">
+                My brain test attempts
+            </h3>
+            <p class="section-description-intentship mt-1 pe-5">
+                You can see latest 10 attempts only. Other older attempts will be deleted automatically.
+            </p>
+            <div class="d-flex flex-row justify-content-between w-100 ">
+               <div class="divtable">
+                {{-- row-1 --}}
+                <div class="d-flex flex-column row1">
+                    <div class="d-flex flex-row justify-content-between">
+                        <p class="title-16-purple" style="font-weight: 700">
+                            Attempt 03
+                        </p>
+                        <p class="title-16-purple" style="font-weight: 500">
+                            21 - 09 - 2023
+                        </p>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="col-3">
+                            <p class="section-description-intentship">
+                                L1 = 86%
+                            </p>
+                        </div>
+                        <div class="col-9">
+                            <p class="section-description-intentship">
+                                L2 = 66%
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="col-3">
+                            <p class="section-description-intentship">
+                                R1 = 52%
+                            </p>
+                        </div>
+                        <div class="col-9">
+                            <p class="section-description-intentship">
+                                R2 = 650
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {{-- row-2 --}}
+                <div class="d-flex flex-column row2">
+                    <div class="d-flex flex-row justify-content-between">
+                        <p class="title-16-purple" style="font-weight: 700">
+                            Attempt 02
+                        </p>
+                        <p class="title-16-purple" style="font-weight: 500">
+                            21 - 09 - 2023
+                        </p>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="col-3">
+                            <p class="section-description-intentship">
+                                L1 = 86%
+                            </p>
+                        </div>
+                        <div class="col-9">
+                            <p class="section-description-intentship">
+                                L2 = 66%
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="col-3">
+                            <p class="section-description-intentship">
+                                R1 = 52%
+                            </p>
+                        </div>
+                        <div class="col-9">
+                            <p class="section-description-intentship">
+                                R2 = 650
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {{-- row-3 --}}
+                <div class="d-flex flex-column row1">
+                    <div class="d-flex flex-row justify-content-between">
+                        <p class="title-16-purple" style="font-weight: 700">
+                            Attempt 03
+                        </p>
+                        <p class="title-16-purple" style="font-weight: 500">
+                            21 - 09 - 2023
+                        </p>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="col-3">
+                            <p class="section-description-intentship">
+                                L1 = 86%
+                            </p>
+                        </div>
+                        <div class="col-9">
+                            <p class="section-description-intentship">
+                                L2 = 66%
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="col-3">
+                            <p class="section-description-intentship">
+                                R1 = 52%
+                            </p>
+                        </div>
+                        <div class="col-9">
+                            <p class="section-description-intentship">
+                                R2 = 650
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+               </div>
+            </div>
+            <div class="d-flex justify-content-end">
+                <button class="yellow-btn mt-4 px-5">Try again</button>
+            </div>
+        </div>
+        <div class="col-5 d-flex justify-content-center align-items-center">
+            <img src="{{ asset('assets/images/brain.PNG') }}" class="img-fluid">
+        </div>
     </div>
 
 </div>
