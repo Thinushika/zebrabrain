adc17be48f9e84319000688a5d1056c768e9c1a3
tj <thinupavani@gmail.com>
2024-03-04 16:21:44 +0530

    update

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -12,7 +12,8 @@
                     <h2 class="mobileMainTitle mb-0">Hi Good Evening!</h2>
                 </div>
                 <div class="col-6 d-flex flex-row d-flex align-items-center">
-                    <div class="col-7 mobileProfileDetails d-flex flex-column justify-content-center align-items-end text-end">
+                    <div
+                        class="col-7 mobileProfileDetails d-flex flex-column justify-content-center align-items-end text-end">
                         <h4>Name</h4>
                         <p>Career name</p>
                     </div>
@@ -26,8 +27,7 @@
                 <input type="text" class="form-control " placeholder="Type what you need to know"
                     style="background: transparent;">
                 <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button"><i
-                            class="fas fa-search"></i></button>
+                    <button class="btn btn-outline-secondary" type="button"><i class="fas fa-search"></i></button>
                 </div>
             </div>
             <h3 class="section-title text-purple mt-5 text-center">Let's check your future opportunities</h3>
@@ -104,30 +104,177 @@
             <h3 class="section-title text-purple mt-5 text-center text-lg-start">Up & coming events
             </h3>
             <p class="section-description mt-2 mt-lg-4">Let's check your event calendar</p>
-            <div class="col-12 col-lg-8">
-                <div id="calendarDiv"></div>
+            <div class="col-12 col-lg-8 pe-3">
+                {{-- <div id="calendarDiv"></div> --}}
+                <style>
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                    }
+
+                    th,
+                    td {
+                        border: 1px solid #dddddd;
+                        text-align: left;
+                        padding: 3px;
+                    }
+
+                    th {
+                        background-color: transparent;
+                        text-align: center
+                    }
+
+                    .task-cell {
+                        text-align: center;
+                        background-color: #5a559d;
+                        border: none;
+                        color: #fff
+                    }
+
+                    .today {
+                        background-color: #5a559d;
+                        color: #fff
+                    }
+                    th{
+                        color: #5a559d;
+                        font-size: 14px;
+                        border: none;
+                    }
+                    th span{
+                        color: #3b3b3b;
+                        font-size: 14px;
+                    }
+                    .selectedTimeOfDay{
+                        background-color: #5a559d;
+                        color: #fff;
+                        border-radius: 100%;
+                        padding: 5px;
+                    }
+                    td{
+                        color: #3b3b3b;
+                        font-size: 12px;
+                    }
+                </style>
+                <table id="timetable">
+                    <tr>
+                        <th></th>
+                        <th>Mon
+                            <br>
+                            <span class="selectedTimeOfDay">20</span>
+                        </th>
+                        <th>Tue
+                            <br>
+                            <span>21</span>
+                        </th>
+                        <th>Wed
+                            <br>
+                            <span>22</span>
+                        </th>
+                        <th>Thu
+                            <br>
+                            <span>23</span>
+                        </th>
+                        <th>Fri
+                            <br>
+                            <span>24</span>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>5:00 - 6:00</td>
+                        <td id="monday_5"></td>
+                        <td id="tuesday_5"></td>
+                        <td id="wednesday_5"></td>
+                        <td id="thursday_5"></td>
+                        <td id="friday_5"></td>
+                    </tr>
+                    <tr>
+                        <td>6:00 - 7:00</td>
+                        <td id="monday_6" class="task-cell"></td>
+                        <td id="tuesday_6" ></td>
+                        <td id="wednesday_6"></td>
+                        <td id="thursday_6"></td>
+                        <td id="friday_6"></td>
+                    </tr>
+                    <tr>
+                        <td>7:00 - 8:00</td>
+                        <td id="monday_7" class="task-cell">Sample Text</td>
+                        <td id="tuesday_7"></td>
+                        <td id="wednesday_7 class="></td>
+                        <td id="thursday_7"></td>
+                        <td id="friday_7"></td>
+                    </tr>
+                    <tr>
+                        <td>8:00 - 9:00</td>
+                        <td id="monday_8" class="task-cell"></td>
+                        <td id="tuesday_8"></td>
+                        <td id="wednesday_8"></td>
+                        <td id="thursday_8"></td>
+                        <td id="friday_8"></td>
+                    </tr>
+                    <tr>
+                        <td>9:00 - 10:00</td>
+                        <td id="monday_9"></td>
+                        <td id="tuesday_9"></td>
+                        <td id="wednesday_9"class="task-cell">Sample Text</td>
+                        <td id="thursday_9"></td>
+                        <td id="friday_9"></td>
+                    </tr>
+                    <tr>
+                        <td>10:00 - 11:00</td>
+                        <td id="monday_10"></td>
+                        <td id="tuesday_10"></td>
+                        <td id="wednesday_10"></td>
+                        <td id="thursday_10"></td>
+                        <td id="friday_10"></td>
+                    </tr>
+                    <tr>
+                        <td>11:00 - 12:00</td>
+                        <td id="monday_11"></td>
+                        <td id="tuesday_11"></td>
+                        <td id="wednesday_11"></td>
+                        <td id="thursday_11"></td>
+                        <td id="friday_11"></td>
+                    </tr>
+                    <tr>
+                        <td>12:00 - 1:00</td>
+                        <td id="monday_12"></td>
+                        <td id="tuesday_12"></td>
+                        <td id="wednesday_12"></td>
+                        <td id="thursday_12"class="task-cell">Sample Text</td>
+                        <td id="friday_12"></td>
+                    </tr>
+                    <tr>
+                        <td>1:00 - 2:00</td>
+                        <td id="monday_1"></td>
+                        <td id="tuesday_1"></td>
+                        <td id="wednesday_1"></td>
+                        <td id="thursday_1"class="task-cell"></td>
+                        <td id="friday_1"></td>
+                    </tr>
+                    <!-- Add more rows as needed -->
+                </table>
             </div>
             <div class="col-12 col-lg-4 hideOnMobile d-flex flex-column justify-content-center align-items-center">
-                <div class="d-flex flex-row">
+                <div class="d-flex flex-row w-100 mb-3">
                     <div class="col-6">
-                        <div id="month-year"></div>
+                        <div id="month-year" class="monthYearTxt"></div>
                     </div>
-                    <div class="col-6 d-flex flex--row">
-                        <button onclick="previousMonth()"><i class="bi bi-arrow-left"></i></button>
-                        <button onclick="nextMonth()"><i class="bi bi-arrow-right"></i></button>
+                    <div class="col-6 d-flex flex--row justify-content-end">
+                        <button onclick="previousMonth()" class="prevBtnCalander"><i class="bi bi-arrow-left"></i></button>
+                        <button onclick="nextMonth()" class="nextBtnCalander"><i class="bi bi-arrow-right"></i></button>
                     </div>
                 </div>
 
                 <table id="calendar">
                     <thead>
                         <tr>
-                            <th>Sun</th>
-                            <th>Mon</th>
-                            <th>Tue</th>
-                            <th>Wed</th>
-                            <th>Thu</th>
-                            <th>Fri</th>
-                            <th>Sat</th>
+                            <th>S</th>
+                            <th>M</th>
+                            <th>T</th>
+                            <th>W</th>
+                            <th>T</th>
+                            <th>F</th>
+                            <th>S</th>
                         </tr>
                     </thead>
                     <tbody id="calendar-body">
@@ -381,7 +528,10 @@
             </p>
             <h3 class="section-title text-purple">Realist</h3>
             <p class="section-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repellat minima cum sapiente ipsa? Autem, provident eveniet ut mollitia sint cumque ducimus, hic qui enim beatae vitae. Provident doloribus laborum assumenda asperiores at excepturi fugit eos mollitia itaque quisquam incidunt, similique debitis quo quod? Explicabo, cum animi! Nulla, odio pariatur!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repellat minima cum sapiente ipsa?
+                Autem, provident eveniet ut mollitia sint cumque ducimus, hic qui enim beatae vitae. Provident doloribus
+                laborum assumenda asperiores at excepturi fugit eos mollitia itaque quisquam incidunt, similique debitis
+                quo quod? Explicabo, cum animi! Nulla, odio pariatur!
             </p>
         </div>
 
@@ -560,7 +710,7 @@
         <div class="row mt-5 mb-5 displayOnMobile">
             <div class="col-12 p-5 text-center d-flex flex-column justify-content-center align-items-center">
                 <h3 class="section-title text-purple">Enroll for <br>
-                Super Futures Club</h3>
+                    Super Futures Club</h3>
                 <button class="yellow-btn mt-4 px-3 px-lg-5">Enroll now</button>
             </div>
         </div>
@@ -570,4 +720,96 @@
 
 
 </div>
+
+<!-- JS for jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- JS for full calender -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+<script>
+    $(document).ready(function() {
+    $('#calendarDiv').fullCalendar({
+    defaultView: 'month',
+    events: [
+      {
+        title: 'sample text',
+        start: '2024-02-01'
+        
+      },
+      {
+        title: 'sample text',
+        start: '2024-02-25'
+      },
+      {
+        title: 'sample text',
+        start: '2024-02-28'
+      }
+      ] 
+    });
+});
+
+
+// Get current date
+const currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+let today = currentDate.getDate();
+
+// Function to display the current month
+function displayCurrentMonth() {
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+document.getElementById('month-year').textContent = months[currentMonth] + ' ' + currentYear;
+
+const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+const daysInMonth = lastDayOfMonth.getDate();
+
+const calendarBody = document.getElementById('calendar-body');
+calendarBody.innerHTML = '';
+
+let date = 1;
+for (let i = 0; i < 6; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < 7; j++) {
+        const cell = document.createElement('td');
+        if (i === 0 && j < firstDayOfMonth.getDay()) {
+            row.appendChild(cell);
+        } else if (date > daysInMonth) {
+            break;
+        } else {
+            cell.textContent = date;
+            row.appendChild(cell);
+            if (date === today) {
+                cell.classList.add('today'); // Add class for today's date
+            }
+            date++;
+        }
+    }
+    calendarBody.appendChild(row);
+}
+}
+
+// Function to navigate to the previous month
+function previousMonth() {
+currentMonth--;
+if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear--;
+}
+displayCurrentMonth();
+}
+
+// Function to navigate to the next month
+function nextMonth() {
+currentMonth++;
+if (currentMonth > 11) {
+    currentMonth = 0;
+    currentYear++;
+}
+displayCurrentMonth();
+}
+
+// Display current month when the page loads
+displayCurrentMonth();
+</script>
 @include('layouts.dashboard-footer')
